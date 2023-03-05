011b0f8751916b34bf0ebf1bb575d5c2bcb949c7
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-03-05 20:49:29 +0530

    Revising

---
+++ Strings.py
@@ -0,0 +1,17 @@
+'''name="Dhruv"
+print(name)
+print(name[0])
+
+s="abcde"
+print (s[1:4])
+
+a="Dhruv"
+for char in a:
+    print(char)
+
+name="superb"
+for char in name:
+    print(char+"-",end="" )'''
+
+word="amazing"
+print(word[0:3])
