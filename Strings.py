c16f43fdbad53f174ebb0406ebf72cb4464f0001
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-03-08 12:19:02 +0530

    Strings Done

--- Strings.py
+++ Strings.py
@@ -11,7 +11,59 @@ for char in a:
 
 name="superb"
 for char in name:
-    print(char+"-",end="" )'''
+    print(char+"-",end="" )
 
 word="amazing"
 print(word[0:3])
+
+for letter in "Hello":
+    print(letter*2,end=" ")
+
+name="Dhruv"
+print(len(name))
+
+str="dhruv"
+print(str.capitalize())
+
+string1 ="abc123"
+string2 ="hello"
+string3 ="123"
+string4 =" "
+print(string1.isalnum())
+print(string2.isalnum())
+print(string3.isalnum())
+print(string4.isalnum())
+
+string1 ="abc123"
+string2 ="hello"
+string3 ="123"
+string4 =" "
+print(string1.isalpha())
+print(string2.isalpha())
+print(string3.isalpha())
+print(string4.isalpha())
+
+string1 ="abc123"
+string2 ="hello"
+string3 ="123"
+string4 =" "
+print(string1.isdigit())
+print(string2.isdigit())
+print(string3.isdigit())
+print(string4.isdigit())
+
+string1 ="abc123"
+string2 ="hello"
+string3 ="123"
+string4 =" "
+print(string1.isspace())
+print(string2.isspace())
+print(string3.isspace())
+print(string4.isspace())'''
+
+string="hello"
+print(string.upper())
+
+string="HELLO"
+print(string.lower())
+
