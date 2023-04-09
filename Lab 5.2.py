01e57abe0e86658d5b9637a20abcdb05a8d2b3e1
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-04-09 18:34:22 +0530

    Lists

--- Lab 5.2.py
+++ Lab 5.2.py
@@ -2,3 +2,13 @@ def odd_numbers(li):
     return [x for x in li if x%2 != 0]
 li=[21,24,26,29,31,45]
 print(odd_numbers(li))
+
+b=[3*i for i in range(10)]
+print(b)
+
+a=[1,2,8,8]
+b=[1,2,8,9]
+if a>b:
+    print("True")
+else:
+    print("False")
---
+++ List.py
@@ -0,0 +1,15 @@
+'''a=[1,2,3]
+print(a)
+
+numbers=[0,1,2,3,4,5]
+print(numbers)
+
+print(numbers[2:5])
+
+print(numbers[-1:0:2])'''
+
+fruits=["Apple","Mango","Guava"]
+'''fruits[2]="Grapes"
+print(fruits)'''
+del fruits[0]
+print(fruits)
