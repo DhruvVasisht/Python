1498f6d058c13d9c0c2f4197e9a0065149023378
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-04-16 19:54:09 +0530

    Python

--- List.py
+++ List.py
@@ -8,8 +8,15 @@ print(numbers[2:5])
 
 print(numbers[-1:0:2])'''
 
-fruits=["Apple","Mango","Guava"]
-'''fruits[2]="Grapes"
-print(fruits)'''
-del fruits[0]
+'''fruits=["Apple","Mango","Guava"]
+fruits[2]="Grapes"
 print(fruits)
+del fruits[0]
+print(fruits)'''
+
+a=[1,2,8,9]
+b=[1,2,7.8]
+if a>b:
+    print("true")
+else:
+    print("false")
