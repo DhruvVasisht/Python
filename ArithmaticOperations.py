3aef1b40e7485a365885323b988a48d01ae34d16
DhruvVasisht <vasisht.dhruv7@gmail.com>
2023-05-24 22:04:41 +0530

    Python

--- ArithmaticOperations.py
+++ ArithmaticOperations.py
@@ -10,11 +10,15 @@ def div(x,y):
 def multiply(x,y):
     return(x*y)
 
+def modulo(x,y):
+    return(x%y)
+
 print("Select An Operation")
 print("1.Add")
 print("2.Sub")
 print("3.Div")
 print("4.Multiply")
+print("5.Modulo")
 
 Operation=int(input("Enter The Choice"))
 x=eval(input("Enter The Number 1 "))
@@ -23,11 +27,14 @@ y=eval(input("Enter The Number 2 "))
 if Operation== 1:
     print(add(x,y))
     
-elif Operation== d2:
+elif Operation== 2:
     print(sub(x,y))
 
 elif Operation== 3:
     print(div(x,y))
 
-else:
+elif Operation==4:
     print(multiply(x,y))
+
+else:
+    print(modulo(x,y))
